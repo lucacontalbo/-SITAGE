289b496cc6f034a51bc97f195263940d20d34607
conta16 <luca.contalbo@gmail.com>
2021-11-14 15:58:12 +0100

    fixed components

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,26 @@
+<template>
+      <q-carousel-slide :name=name :img-src=picture>
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">First stop</div>
+          <div class="text-subtitle1">Mountains</div>
+        </div>
+      </q-carousel-slide>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CarouselComponent',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    picture:{
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
