8854b89f2229a8cc4638cafcccc358812d616ae6
conta16 <luca.contalbo@gmail.com>
2021-12-11 16:18:09 +0100

    added access token automatic retrieval

--- sh/access_token_refresh.sh
+++ sh/access_token_refresh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-value=$(jq '.access_token' ./accesstoken.json)
-#echo ${value:1:-1} > ./prova.json
+value=$(jq '.access_token' ./accesstokenold.json)
 curl -X GET https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${value:1:-1} > ./accesstoken.json
 
--- src/components/IgComponent.vue
+++ src/components/IgComponent.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="cont">
     <div class="q-pt-lg q-mt-lg q-mb-lg q-pa-sx row items-start justify-center">
-      <div class="q-pa-md row items-start q-gutter-md">
+      <div v-for='(url,index) in urlImages' :key='index' v-bind='url'>
+      <div class="q-pa-md row items-stretch q-gutter-md">
+        <!--TODO: keep q-cards sizes fixed. (e.g. when loading, the size dinamically changes based on the num of cards loaded)-->
         <q-card class="card-ig" flat bordered>
-          <q-img src="/ig/0_ig.jpg" />
+          <q-img :src="url.media_url" />
 
           <q-card-section>
             <div class="text-caption text-grey">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              {{url.caption}}
             </div>
           </q-card-section>
 
@@ -35,80 +36,13 @@
           </q-slide-transition>
         </q-card>
       </div>
-      <div class="q-pa-md row items-start q-gutter-md">
-        <q-card class="card-ig" flat bordered>
-          <q-img src="/ig/1_ig.jpg" />
-
-          <q-card-section>
-            <div class="text-caption text-grey">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-          </q-card-section>
-
-          <q-card-actions>
-            <q-space />
-
-            <q-btn
-              color="grey"
-              round
-              flat
-              dense
-              :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-              @click="expanded = !expanded"
-            />
-          </q-card-actions>
-
-          <q-slide-transition>
-            <div v-show="expanded">
-              <q-separator />
-              <q-card-section class="text-subitle2">
-                Lorem ipsum di prova
-              </q-card-section>
-            </div>
-          </q-slide-transition>
-        </q-card>
-      </div>
-      <div class="q-pa-md row items-start q-gutter-md">
-        <q-card class="card-ig" flat bordered>
-          <q-img src="/ig/2_ig.jpg" />
-
-          <q-card-section>
-            <div class="text-caption text-grey">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </div>
-          </q-card-section>
-
-          <q-card-actions>
-            <q-space />
-
-            <q-btn
-              color="grey"
-              round
-              flat
-              dense
-              :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-              @click="expanded = !expanded"
-            />
-          </q-card-actions>
-
-          <q-slide-transition>
-            <div v-show="expanded">
-              <q-separator />
-              <q-card-section class="text-subitle2">
-                Lorem ipsum di prova
-              </q-card-section>
-            </div>
-          </q-slide-transition>
-        </q-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 
 export default defineComponent({
   name: "ListItemComponent",
@@ -119,8 +53,24 @@ export default defineComponent({
     },
   },
   setup() {
+    const expanded = ref(false)
+    const urlImages = ref([])
+    onMounted(async () => {
+      const { access_token } = process.env.NODE_ENV === 'production' ? (await import(
+      /* webpackIgnore: true */ '/accesstoken.json'
+    )).default : await import('app/public/accesstoken.json')
+      const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption&access_token=${access_token}`)
+      const {data:igIdx} = await response.json()
+      for (let j=0; j<3; j++){
+        const urlsObj = await fetch('https://graph.instagram.com/'+igIdx[j]["id"]+'/?fields=media_url,caption&access_token=IGQVJVNHBWTE5COUdYY2U2UTFIMVF6cHJHWEpCZAGYwSTJ3YXdwWFVmOW44UzhnX2hzMWlONl90N1pMZAGlodC1WaUtmc01zRGN0d3pGck5IMmRLamtGVHdsNmU2ZA1JaYy1qV3Fsb1lLcmQ2Vl9mUk0xWQZDZD')
+        const url = await urlsObj.json()
+        urlImages.value.push(url)
+      }
+      console.log(urlImages.value)
+    });
     return {
-      expanded: ref(false),
+      expanded,
+      urlImages
     };
   },
 });
@@ -129,6 +79,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 .card-ig {
   width: 100%;
+  //height: 30vh;
   max-width: 350px;
 }
 
